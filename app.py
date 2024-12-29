fe10a5af88a3b52b26842247f283f4b67b38daf6
codewithgabriel <olatoyeolaide@gmail.com>
2024-12-29 08:04:28 -0600

    hacking streamlit to host flask app

--- app.py
+++ app.py
@@ -1,59 +1,31 @@
-from flask import Flask, request, jsonify , render_template
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import numpy as np
-import os
-from PIL import Image
+import streamlit as st
 
-app = Flask(__name__)
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
 
-# Load your pre-trained model
-model_path = os.path.join('static/model' , 'custom-glaucoma-model.h5')
-model = load_model(model_path)
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Check if an image was uploaded
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image uploaded'}), 400
+    from flask import Flask
 
-    img = request.files['image']
+    app = Flask(__name__)
 
-    # Save the image temporarily
-    img_path = os.path.join('static/temp', img.filename)
-    img.save(img_path)
+    @app.route('/foo')
+    def serve_foo():
+        return 'This page is served via Flask!'
 
-    # Preprocess the image
-    img = Image.open(img_path).resize((32, 32))  # Adjust size according to your model
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-
-    # Normalize the image
-    img_array = img_array / 255.0
-
-    # Predict using the model
-    prediction = model.predict(img_array)
-    # print(prediction)
-    predicted_class = np.argmax(prediction, axis=1)
-
-    # Clean up the temporary image
-    os.remove(img_path)
-    print(prediction)
-    
-    prediction = prediction[0]
+    app.run(port=8888)
 
 
-        
-    result = {
-        'nrg' : str(prediction[0]),
-        'rg': str(prediction[1]),
-    }
-    print(result)
-    # Return the prediction
-    return jsonify(result)
-if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0")
+# We'll never reach this part of the code the first time this file executes!
+
+# Your normal Streamlit app goes here:
+x = st.slider('Pick a number')
+st.write('You picked:', x)
